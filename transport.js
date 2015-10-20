e98269b085976c4cb59e15e96cd864a36c1388c8
Muhammed Thanish <mnmtanish@gmail.com>
2015-10-20 09:06:38 +0530

    Add nodejs version of transport

---
+++ transport.js
@@ -0,0 +1,171 @@
+var net = require('net');
+var url = require('url');
+
+function Connection(socket){
+  this._socket = socket;
+  this._data = new Buffer(8192);
+  this._dataTmp = new Buffer(8192);
+  this._cursor = 0;
+  this._items = [];
+  this._batches = [];
+  this._readBatchCallbacks = [];
+  this._dataEnd = 0;
+  this._socket.on('data', this._onData.bind(this));
+}
+
+function dial(address, callback){
+  var socket = new net.Socket();
+
+  function onConnect () {
+    var connection = new Connection(socket);
+    callback(null, connection);
+  }
+
+  function onError (err) {
+    console.log(err);
+    socket.removeListener('connect', onConnect);
+    callback(err);
+  }
+
+  address = url.parse(address);
+  socket.once('connect', onConnect);
+  socket.once('error', onError);
+  socket.connect(address.port, address.hostname);
+}
+
+Connection.prototype.write = function(data, callback){
+    this._socket.write(data, 'binary', callback);
+}
+
+Connection.prototype._onData = function(data){
+  var newSize = this._dataEnd + data.length;
+  if(this._data.length < newSize) {
+    this._growBuffer(newSize);
+  }
+
+  data.copy(this._data, this._dataEnd);
+  var buffer = this._data.slice(0, newSize);
+
+  var startOffset = 0;
+
+  while(true){
+    if(!this._current){ // Header has not been read
+      if(buffer.length-startOffset < 13){
+        // Header is not received yet
+        break;
+      }
+
+      var id = buffer.readUInt32LE(startOffset)
+      var type = buffer.readUInt8(startOffset+8)
+      var size = buffer.readUInt32LE(startOffset+9)
+
+      this._current = {
+        id: id,
+        type: type,
+        size: size,
+        read: 0
+      }
+
+      startOffset = startOffset + 13
+    }
+
+    while(this._current.size>this._current.read){
+      if(!this._current.item){ // Not in the middle of reading an item
+        // Start reading a new item
+        if(buffer.length-startOffset < 4){
+          break;
+        }
+
+        var itemSize = buffer.readUInt32LE(startOffset);
+        startOffset += 4;
+        this._current.item = {
+          size: itemSize
+        }
+      }
+
+      if(buffer.length-startOffset < this._current.item.size){ // Current item is not received yet
+        break;
+      }
+
+      var item = this._current.item;
+
+      var itemBuf = buffer.slice(startOffset, startOffset+item.size);
+      startOffset = startOffset + item.size;
+
+      this._items.push(itemBuf);
+
+      this._current.read++;
+      this._current.item = null;
+    }
+
+    if(this._current.size>this._current.read){
+      // All messages are not read, need more data
+      break;
+    }
+
+    // Call registered callbacks
+    if(this._readBatchCallbacks.length){
+      // there are callbacks call the first
+      var cb = this._readBatchCallbacks.pop();
+      cb(this._current.id, this._current.type, this._items);
+    } else {
+      this._batches.push({
+        id: this._current.id,
+        type: this._current.type,
+        data: this._items
+      });
+    }
+
+    this._items = [];
+    this._current = null
+  }
+
+  // set remaining data as this._data
+  buffer.copy(this._dataTmp, 0, startOffset);
+  this._dataEnd = buffer.length - startOffset;
+
+  // swap buffers
+  var tmpBuff = this._data;
+  this._data = this._dataTmp;
+  this._dataTmp = tmpBuff;
+}
+
+Connection.prototype._growBuffer = function(size) {
+  var buff = new Buffer(size);
+  this._data.copy(buff);
+  this._data = buff;
+};
+
+function Transport(connection){
+  this._connection = connection
+  this._readBatchCallbacks = []
+}
+
+Transport.prototype.ReadBatch = function(callback){
+  if(this._connection._batches.length){
+    var batch = this._connection._batches.pop()
+    callback(batch.id, batch.type, batch.data);
+  } else {
+    this._connection._readBatchCallbacks.push(callback);
+  }
+}
+
+Transport.prototype.WriteBatch = function(id, type, reqBatch){
+  var header = new Buffer(13);
+  var itemSizeBuf = new Buffer(4);
+  header.fill(0)
+  header.writeUInt32LE(id, 0);
+  header.writeUInt8(type, 8);
+  header.writeUInt32LE(reqBatch.length, 9)
+  this._connection.write(header)
+
+  var self = this
+  reqBatch.forEach(function(item){
+    itemSizeBuf.writeUInt32LE(item.length)
+    self._connection.write(itemSizeBuf)
+    self._connection.write(item)
+  })
+}
+
+module.exports.dial = dial;
+module.exports.Transport = Transport;
